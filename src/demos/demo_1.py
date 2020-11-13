9fbe2bbc19f311548e2260669ec03bdc8cdde518
Toby Chen <toby-chen@lambdastudents.comclear>
2020-11-13 00:08:03 -0500

    finished demos

--- src/demos/demo_1.py
+++ src/demos/demo_1.py
@@ -16,14 +16,26 @@ delete_node(y)
 ```
 *Note: We can do this in O(1) time and space! But be aware that our solution will have some side effects...*
 """
+
+
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
-        self.next  = None
+        self.next = None
+
 
 def delete_node(delete_this_node):
     # Your code here
-    pass
+    # set my next to the node that i want to delete(s) next
+    next = delete_this_node.next
+    # does this node exist?
+    if next:
+        # set this nodes value to the value of the next node
+        delete_this_node.value = next.value
+        # set this nodes next to its next(s) next
+        delete_this_node.next = next.next
+    else:
+        raise Exception("This technique deose not work for the last node in the list")
 
 
 x = LinkedListNode('X')
--- src/demos/demo_2.py
+++ src/demos/demo_2.py
@@ -10,11 +10,33 @@ once.
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
 """
+
+
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
-        self.next  = None
+        self.next = None
+
 
 def reverse(head_of_list):
     # Your code here
-    pass
+    # create a ref to the head of list (our current node)
+    current_node = head_of_list
+    # create a pointer for the prev (set to null, None, nullptr)
+    prev = None
+    # create a pointer for a next (set to null, None, nullptr)
+    next = None
+
+    # while the current node exists (traverse the linked list while the current node is not none)
+    while current_node:
+        # my next pointer gets set to the current nodes `next`
+        next = current_node.next
+        # my current nodes next gets set to the prev
+        current_node.next = prev
+        # set my prev pointer to my current node
+        prev = current_node
+        # set ref of current to next (increment to the next node in the chain)
+        current_node = next
+
+    # return the node that the prev pointer is currently pointing to
+    return prev
